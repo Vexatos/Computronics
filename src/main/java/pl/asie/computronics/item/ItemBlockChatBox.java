2f409a467cac827f9e20962d900009555a390016
Techokami <techokami@holenet.info>
2014-06-24 23:26:13 -0400

    Restored Creative Chat Box... sort of.
    I herped and I derped and I forgot to bring this block over when porting
    the update. HOWEVER for some reason I can't get it to show up in NEI or
    the Creative mode inventory, but you CAN /give it to yourself.
    Uncommenting line 24 in ItemBlockChatBox.java will cause compile to
    fail... but this is probably where the problem is.

---
+++ src/main/java/pl/asie/computronics/item/ItemBlockChatBox.java
@@ -0,0 +1,40 @@
+package pl.asie.computronics.item;
+
+import java.util.List;
+
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.block.Block;
+import net.minecraft.util.EnumChatFormatting;
+import pl.asie.computronics.Computronics;
+
+public class ItemBlockChatBox extends ItemBlock {
+	public ItemBlockChatBox(Block block) {
+		super(block);
+		this.setHasSubtypes(true);
+	}
+	
+	//@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubItems(int id, CreativeTabs tab, List list) {
+		list.add(new ItemStack(this, 1, 0));
+		if(Computronics.CHATBOX_CREATIVE) list.add(new ItemStack(this, 1, 8));
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean wat) {
+		if(stack.getItemDamage() >= 8) list.add(EnumChatFormatting.GRAY + I18n.format("tooltip.computronics.chatBox.creative"));
+	}
+	
+	@Override
+	public String getUnlocalizedName(ItemStack stack) {
+		return (stack.getItemDamage() >= 8 ? "tile.computronics.chatBox.creative" : "tile.computronics.chatBox");
+	}
+}
