f39d79eb3992ee9f889d67657005d27e23bcbf46
Techokami <techokami@holenet.info>
2014-07-06 09:52:08 -0400

    Radars need power now
    It is configurable, too! They suck down a fat wad of power each
    operation (and it's doubled if you scan for all entities instead of just
    players or just mobs!), and if there isn't enough power, all you get
    back is an empty array.

--- src/main/java/pl/asie/computronics/Computronics.java
+++ src/main/java/pl/asie/computronics/Computronics.java
@@ -81,6 +81,7 @@ public class Computronics {
 	public static int BUFFER_MS = 750;
 	public static int RADAR_RANGE = 8;
 	public static boolean RADAR_ONLY_DISTANCE = false;
+	public static double RADAR_ENERGY_COST = 5.0;
 	public static String CHATBOX_PREFIX = "ChatBox";
 
 	public static String TAPE_LENGTHS;
@@ -131,6 +132,7 @@ public class Computronics {
 		TAPE_LENGTHS = config.get("tapedrive", "tapeLengths", "4,8,16,32,64,2,6,16,128").getString();
 		RADAR_RANGE = config.get("radar", "maxRange", 8).getInt();
 		RADAR_ONLY_DISTANCE = config.get("radar", "onlyOutputDistance", false).getBoolean(false);
+		RADAR_ENERGY_COST = config.get("energy", "radarCost", 5.0).getDouble();
 
 		config.get("camera", "sendRedstoneSignal", true).comment = "Setting this to false might help Camera tick lag issues, at the cost of making them useless with redstone circuitry.";
 		
