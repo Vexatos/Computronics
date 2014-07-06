c386eec22ecbedb11872d75c248a3c5138104161
asiekierka <kontakt@asie.pl>
2014-07-06 10:26:38 +0200

    clean up Radar code, (@Techokami - youre free to work on it now)

--- src/main/java/pl/asie/computronics/tile/TileRadar.java
+++ src/main/java/pl/asie/computronics/tile/TileRadar.java
@@ -10,6 +10,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.AxisAlignedBB;
 import pl.asie.lib.block.TileEntityBase;
 import pl.asie.computronics.Computronics;
+import pl.asie.computronics.util.RadarUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,75 +18,24 @@ import java.util.List;
 import java.util.Map;
 
 public class TileRadar extends TileEntityBase implements SimpleComponent {
-    public static final double RadarRange = Computronics.RADAR_RANGE;
 
     @Override
     public String getComponentName() {
         return "radar";
     }
 
+    private AxisAlignedBB getBounds() {
+    	return AxisAlignedBB.
+                getBoundingBox(xCoord, yCoord, zCoord, xCoord + 1, yCoord + 1, zCoord + 1).
+                expand(Computronics.RADAR_RANGE, Computronics.RADAR_RANGE, Computronics.RADAR_RANGE);
+    }
     @Callback
     public Object[] getEntities(Context context, Arguments args) {
         List<Map> entities = new ArrayList<Map>();
-        AxisAlignedBB bounds = AxisAlignedBB.
-                getBoundingBox(xCoord, yCoord, zCoord, xCoord + 1, yCoord + 1, zCoord + 1).
-                expand(RadarRange, RadarRange, RadarRange);
-		//Get players first
-		for (Object obj : getWorldObj().getEntitiesWithinAABB(EntityPlayer.class, bounds)) {
-            EntityPlayer entity = (EntityPlayer) obj;
-            double dx = entity.posX - (xCoord + 0.5);
-			double dy = entity.posY - (yCoord + 0.5);
-            double dz = entity.posZ - (zCoord + 0.5);
-            // Check if the entity is actually in range. I forget, is there a cubed root function?
-            if (Math.sqrt(dx * dx + dz * dz) < RadarRange && Math.sqrt(dx * dx + dy * dy) < RadarRange && Math.sqrt(dy * dy + dz * dz) < RadarRange) {
-                // Maps are converted to tables on the Lua side.
-                Map<String, Object> entry = new HashMap<String, Object>();
-                entry.put("name", entity.getDisplayName());
-                entry.put("x", (int) dx);
-				entry.put("y", (int) dy);
-                entry.put("z", (int) dz);
-                entities.add(entry);
-            }
-        }
-		//Get robots second
-		for (Object obj : getWorldObj().getEntitiesWithinAABB(Robot.class, bounds)) {
-            EntityPlayer entity = ((Robot) obj).player();
-            double dx = entity.posX - (xCoord + 0.5);
-			double dy = entity.posY - (yCoord + 0.5);
-            double dz = entity.posZ - (zCoord + 0.5);
-            // Check if the entity is actually in range. I forget, is there a cubed root function?
-            if (Math.sqrt(dx * dx + dz * dz) < RadarRange && Math.sqrt(dx * dx + dy * dy) < RadarRange && Math.sqrt(dy * dy + dz * dz) < RadarRange) {
-                // Maps are converted to tables on the Lua side.
-                Map<String, Object> entry = new HashMap<String, Object>();
-                entry.put("name", entity.getDisplayName());
-                entry.put("x", (int) dx);
-				entry.put("y", (int) dy);
-                entry.put("z", (int) dz);
-                entities.add(entry);
-            }
-        }
-		//Get mobs last
-        for (Object obj : getWorldObj().getEntitiesWithinAABB(EntityLiving.class, bounds)) {
-            EntityLiving entity = (EntityLiving) obj;
-            double dx = entity.posX - (xCoord + 0.5);
-			double dy = entity.posY - (yCoord + 0.5);
-            double dz = entity.posZ - (zCoord + 0.5);
-            // Check if the entity is actually in range. I forget, is there a cubed root function?
-            if (Math.sqrt(dx * dx + dz * dz) < RadarRange && Math.sqrt(dx * dx + dy * dy) < RadarRange && Math.sqrt(dy * dy + dz * dz) < RadarRange) {
-                // Maps are converted to tables on the Lua side.
-                Map<String, Object> entry = new HashMap<String, Object>();
-                if (entity.hasCustomNameTag()) {
-                    entry.put("name", entity.getCustomNameTag());
-                }
-                else {
-                    entry.put("name", entity.getCommandSenderName());
-                }
-                entry.put("x", (int) dx);
-				entry.put("y", (int) dy);
-                entry.put("z", (int) dz);
-                entities.add(entry);
-            }
-        }
+        AxisAlignedBB bounds = getBounds();
+        entities.addAll(RadarUtils.getEntities(getWorldObj(), xCoord, yCoord, zCoord, bounds, EntityPlayer.class));
+        entities.addAll(RadarUtils.getEntities(getWorldObj(), xCoord, yCoord, zCoord, bounds, Robot.class));
+        entities.addAll(RadarUtils.getEntities(getWorldObj(), xCoord, yCoord, zCoord, bounds, EntityLiving.class));
         context.pause(0.5);
 
         // The returned array is treated as a tuple, meaning if we return the
@@ -100,107 +50,30 @@ public class TileRadar extends TileEntityBase implements SimpleComponent {
 	@Callback
     public Object[] getPlayers(Context context, Arguments args) {
         List<Map> entities = new ArrayList<Map>();
-        AxisAlignedBB bounds = AxisAlignedBB.
-                getBoundingBox(xCoord, yCoord, zCoord, xCoord + 1, yCoord + 1, zCoord + 1).
-                expand(RadarRange, RadarRange, RadarRange);
-		//Get players
-		for (Object obj : getWorldObj().getEntitiesWithinAABB(EntityPlayer.class, bounds)) {
-            EntityPlayer entity = (EntityPlayer) obj;
-            double dx = entity.posX - (xCoord + 0.5);
-			double dy = entity.posY - (yCoord + 0.5);
-            double dz = entity.posZ - (zCoord + 0.5);
-            // Check if the entity is actually in range. I forget, is there a cubed root function?
-            if (Math.sqrt(dx * dx + dz * dz) < RadarRange && Math.sqrt(dx * dx + dy * dy) < RadarRange && Math.sqrt(dy * dy + dz * dz) < RadarRange) {
-                // Maps are converted to tables on the Lua side.
-                Map<String, Object> entry = new HashMap<String, Object>();
-                entry.put("name", entity.getDisplayName());
-                entry.put("x", (int) dx);
-				entry.put("y", (int) dy);
-                entry.put("z", (int) dz);
-                entities.add(entry);
-            }
-        }
+        AxisAlignedBB bounds = getBounds();
+        entities.addAll(RadarUtils.getEntities(getWorldObj(), xCoord, yCoord, zCoord, bounds, EntityPlayer.class));
         context.pause(0.5);
-
-        // The returned array is treated as a tuple, meaning if we return the
-        // entities as an array directly, we'd end up with each entity as an
-        // individual result value (i.e. in Lua we'd have to write
-        //   result = {radar.getEntities()}
-        // and we'd be limited in the number of entities, due to the limit of
-        // return values. So we wrap it in an array to return it as a list.
+        
         return new Object[]{entities.toArray()};
     }
 	
 	@Callback
     public Object[] getRobots(Context context, Arguments args) {
         List<Map> entities = new ArrayList<Map>();
-        AxisAlignedBB bounds = AxisAlignedBB.
-                getBoundingBox(xCoord, yCoord, zCoord, xCoord + 1, yCoord + 1, zCoord + 1).
-                expand(RadarRange, RadarRange, RadarRange);
-		//Get robots
-		for (Object obj : getWorldObj().getEntitiesWithinAABB(Robot.class, bounds)) {
-            EntityPlayer entity = ((Robot) obj).player();
-            double dx = entity.posX - (xCoord + 0.5);
-			double dy = entity.posY - (yCoord + 0.5);
-            double dz = entity.posZ - (zCoord + 0.5);
-            // Check if the entity is actually in range. I forget, is there a cubed root function?
-            if (Math.sqrt(dx * dx + dz * dz) < RadarRange && Math.sqrt(dx * dx + dy * dy) < RadarRange && Math.sqrt(dy * dy + dz * dz) < RadarRange) {
-                // Maps are converted to tables on the Lua side.
-                Map<String, Object> entry = new HashMap<String, Object>();
-                entry.put("name", entity.getDisplayName());
-                entry.put("x", (int) dx);
-				entry.put("y", (int) dy);
-                entry.put("z", (int) dz);
-                entities.add(entry);
-            }
-        }
+        AxisAlignedBB bounds = getBounds();
+        entities.addAll(RadarUtils.getEntities(getWorldObj(), xCoord, yCoord, zCoord, bounds, Robot.class));
         context.pause(0.5);
 
-        // The returned array is treated as a tuple, meaning if we return the
-        // entities as an array directly, we'd end up with each entity as an
-        // individual result value (i.e. in Lua we'd have to write
-        //   result = {radar.getEntities()}
-        // and we'd be limited in the number of entities, due to the limit of
-        // return values. So we wrap it in an array to return it as a list.
         return new Object[]{entities.toArray()};
     }
 	
 	@Callback
     public Object[] getMobs(Context context, Arguments args) {
         List<Map> entities = new ArrayList<Map>();
-        AxisAlignedBB bounds = AxisAlignedBB.
-                getBoundingBox(xCoord, yCoord, zCoord, xCoord + 1, yCoord + 1, zCoord + 1).
-                expand(RadarRange, RadarRange, RadarRange);		
-		//Get mobs
-        for (Object obj : getWorldObj().getEntitiesWithinAABB(EntityLiving.class, bounds)) {
-            EntityLiving entity = (EntityLiving) obj;
-            double dx = entity.posX - (xCoord + 0.5);
-			double dy = entity.posY - (yCoord + 0.5);
-            double dz = entity.posZ - (zCoord + 0.5);
-            // Check if the entity is actually in range. I forget, is there a cubed root function?
-            if (Math.sqrt(dx * dx + dz * dz) < RadarRange && Math.sqrt(dx * dx + dy * dy) < RadarRange && Math.sqrt(dy * dy + dz * dz) < RadarRange) {
-                // Maps are converted to tables on the Lua side.
-                Map<String, Object> entry = new HashMap<String, Object>();
-                if (entity.hasCustomNameTag()) {
-                    entry.put("name", entity.getCustomNameTag());
-                }
-                else {
-                    entry.put("name", entity.getCommandSenderName());
-                }
-                entry.put("x", (int) dx);
-				entry.put("y", (int) dy);
-                entry.put("z", (int) dz);
-                entities.add(entry);
-            }
-        }
+        AxisAlignedBB bounds = getBounds();
+        entities.addAll(RadarUtils.getEntities(getWorldObj(), xCoord, yCoord, zCoord, bounds, EntityLiving.class));
         context.pause(0.5);
-
-        // The returned array is treated as a tuple, meaning if we return the
-        // entities as an array directly, we'd end up with each entity as an
-        // individual result value (i.e. in Lua we'd have to write
-        //   result = {radar.getEntities()}
-        // and we'd be limited in the number of entities, due to the limit of
-        // return values. So we wrap it in an array to return it as a list.
+        
         return new Object[]{entities.toArray()};
     }
 }
---
+++ src/main/java/pl/asie/computronics/util/RadarUtils.java
@@ -0,0 +1,46 @@
+package pl.asie.computronics.util;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import li.cil.oc.api.machine.Robot;
+import pl.asie.computronics.Computronics;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
+
+public class RadarUtils {
+	public static Set<Map> getEntities(World world, int xCoord, int yCoord ,int zCoord, AxisAlignedBB bounds, Class eClass) {
+		Set<Map> entities = new HashSet<Map>();
+		for (Object obj : world.getEntitiesWithinAABB(eClass, bounds)) {
+            EntityLivingBase entity;
+            if(eClass.equals(Robot.class)) {
+            	entity = ((Robot)obj).player();
+            } else entity = (EntityLivingBase)obj;
+            double dx = entity.posX - (xCoord + 0.5);
+			double dy = entity.posY - (yCoord + 0.5);
+            double dz = entity.posZ - (zCoord + 0.5);
+            // Check if the entity is actually in range. I forget, is there a cubed root function?
+            if (Math.sqrt(dx * dx + dz * dz) < Computronics.RADAR_RANGE && Math.sqrt(dx * dx + dy * dy) < Computronics.RADAR_RANGE && Math.sqrt(dy * dy + dz * dz) < Computronics.RADAR_RANGE) {
+                // Maps are converted to tables on the Lua side.
+                Map<String, Object> entry = new HashMap<String, Object>();
+                if(entity instanceof EntityPlayer) {
+                    entry.put("name", ((EntityPlayer)entity).getDisplayName());
+                } else if (entity instanceof EntityLiving && ((EntityLiving)entity).hasCustomNameTag()) {
+                    entry.put("name", ((EntityLiving)entity).getCustomNameTag());
+                } else {
+                    entry.put("name", entity.getCommandSenderName());
+                }
+                entry.put("x", (int) dx);
+				entry.put("y", (int) dy);
+                entry.put("z", (int) dz);
+                entities.add(entry);
+            }
+        }
+		return entities;
+	}
+}
