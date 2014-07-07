16e2263f32ece254a905e8ca5f354a9a2161ac79
Techokami <techokami@holenet.info>
2014-07-07 12:41:33 -0400

    Particle Effects Card is DONE!
    Though I did move that Utils class contents into the main class, because
    otherwise they wouldn't actually work. If you want to move things back,
    make sure Minecraft can still actually use them :V

--- src/main/java/pl/asie/computronics/Computronics.java
+++ src/main/java/pl/asie/computronics/Computronics.java
@@ -64,6 +64,12 @@ import cpw.mods.fml.common.network.FMLEventChannel;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import dan200.computercraft.api.ComputerCraftAPI;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.client.Minecraft;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent;
+import cpw.mods.fml.common.network.internal.FMLProxyPacket;
 
 @Mod(modid="computronics", name="Computronics", version="0.5.0", dependencies="required-after:asielib;after:ComputerCraft;after:OpenComputers;after:OpenComputers|Core;after:BuildCraft|Core")
 public class Computronics {
@@ -269,5 +275,32 @@ public class Computronics {
 		Computronics.storage = new StorageManager();
 	}
 	
+	public static void sendParticlePacket(String name, int dimension, double x, double y, double z, double vx, double vy, double vz) {
+        ByteBuf data = Unpooled.buffer();
+        byte[] nameBytes = name.getBytes();
+        data.writeShort(nameBytes.length);
+        data.writeBytes(nameBytes);
+        data.writeFloat((float) x);
+        data.writeFloat((float) y);
+        data.writeFloat((float) z);
+        data.writeFloat((float) vx);
+        data.writeFloat((float) vy);
+        data.writeFloat((float) vz);
+        channel.sendToAllAround(new FMLProxyPacket(data, "Computronics"), new NetworkRegistry.TargetPoint(dimension, x, y, z, 64));
+    }
+
+    @SubscribeEvent
+    public void onPacket(FMLNetworkEvent.ClientCustomPacketEvent event) {
+        ByteBuf data = event.packet.payload();
+        int nameLength = data.readShort();
+        String name = new String(data.readBytes(nameLength).array());
+        double x = data.readFloat();
+        double y = data.readFloat();
+        double z = data.readFloat();
+        double vx = data.readFloat();
+        double vy = data.readFloat();
+        double vz = data.readFloat();
+        Minecraft.getMinecraft().thePlayer.getEntityWorld().spawnParticle(name, x, y, z, vx, vy, vz);
+    }
 	
 }
