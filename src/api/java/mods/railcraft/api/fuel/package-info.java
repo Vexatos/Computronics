8277ae721e83cbb44396c4b46784dd26c3493ec8
Vexatos <Stuarzt@gmx.de>
2014-08-16 14:20:20 +0200

    Removed Railcraft API properly.

--- src/api/java/mods/railcraft/api/fuel/package-info.java
+++
@@ -1,8 +0,0 @@
-/*******************************************************************************
- * Copyright 2011-2014 CovertJaguar
- * 
- * This work (the API) is licensed under the "MIT" License, see LICENSE.txt for details.
- ******************************************************************************/
-@API(apiVersion="1.0", owner="RailcraftAPI|core", provides="RailcraftAPI|fuel")
-package mods.railcraft.api.fuel;
-import cpw.mods.fml.common.API;
--- src/api/java/mods/railcraft/api/signals/package-info.java
+++
@@ -1,8 +0,0 @@
-/*******************************************************************************
- * Copyright 2011-2014 CovertJaguar
- * 
- * This work (the API) is licensed under the "MIT" License, see LICENSE.txt for details.
- ******************************************************************************/
-@API(apiVersion="1.1", owner="RailcraftAPI|core", provides="RailcraftAPI|signals")
-package mods.railcraft.api.signals;
-import cpw.mods.fml.common.API;
