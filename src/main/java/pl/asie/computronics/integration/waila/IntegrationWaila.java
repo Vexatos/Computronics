a7f1e03680d0319c8d85ad88a17d08dee1b9a943
Vexatos <Stuarzt@gmx.de>
2014-10-18 19:55:39 +0200

    Removed broken Waila integration for now

--- src/main/java/pl/asie/computronics/integration/waila/IntegrationWaila.java
+++ src/main/java/pl/asie/computronics/integration/waila/IntegrationWaila.java
@@ -1,24 +1,20 @@
 package pl.asie.computronics.integration.waila;
 
-import cpw.mods.fml.common.Loader;
 import mcp.mobius.waila.api.IWailaRegistrar;
-import pl.asie.computronics.reference.Mods;
-import pl.asie.computronics.tile.TileEntityPeripheralBase;
-import pl.asie.computronics.tile.TileLocomotiveRelay;
 import pl.asie.computronics.tile.TileTapeDrive;
 
 public class IntegrationWaila {
 	public static void register(IWailaRegistrar reg) {
-		reg.registerBodyProvider(new WailaPeripheralBase(), TileEntityPeripheralBase.class);
-		registerKeys(reg, TileEntityPeripheralBase.class, "oc:node");
+		//reg.registerBodyProvider(new WailaPeripheralBase(), TileEntityPeripheralBase.class);
+		//registerKeys(reg, TileEntityPeripheralBase.class, "oc:node");
 
 		reg.registerBodyProvider(new WailaTapeDrive(), TileTapeDrive.class);
 		//registerKeys(reg, TileTapeDrive.class, "*");
 
-		if(Loader.isModLoaded(Mods.Railcraft)) {
+		/*if(Loader.isModLoaded(Mods.Railcraft)) {
 			reg.registerBodyProvider(new WailaLocomotiveRelay(), TileLocomotiveRelay.class);
 			registerKeys(reg, TileLocomotiveRelay.class, "*");
-		}
+		}*/
 
 		ConfigValues.registerConfigs(reg);
 	}
