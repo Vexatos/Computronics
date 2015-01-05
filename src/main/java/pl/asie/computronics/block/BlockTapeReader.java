252a45eb67eb2f01e920956d7376a855e11a6c21
Vexatos <Stuarzt@gmx.de>
2015-01-05 09:09:04 +0100

    Added comparator output to tape drives. Closes #97.

--- src/main/java/pl/asie/computronics/block/BlockTapeReader.java
+++ src/main/java/pl/asie/computronics/block/BlockTapeReader.java
@@ -7,37 +7,52 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import pl.asie.computronics.tile.TapeDriveState;
 import pl.asie.computronics.tile.TileTapeDrive;
 
 public class BlockTapeReader extends BlockMachineSidedIcon {
 	private IIcon mFront;
-	
+
 	public BlockTapeReader() {
 		super();
 		this.setBlockName("computronics.tapeDrive");
 		this.setGuiID(0);
 	}
-	
+
 	@Override
 	public TileEntity createNewTileEntity(World world, int metadata) {
 		return new TileTapeDrive();
 	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
 	public IIcon getAbsoluteSideIcon(int sideNumber, int metadata) {
 		return sideNumber == 2 ? mFront : super.getAbsoluteSideIcon(sideNumber, metadata);
 	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerBlockIcons(IIconRegister r) {
 		super.registerBlockIcons(r);
 		mFront = r.registerIcon("computronics:tape_drive_front");
 	}
-	
+
 	@Override
 	public boolean receivesRedstone(IBlockAccess world, int x, int y, int z) {
 		return true;
+	}
+
+	@Override
+	public boolean hasComparatorInputOverride() {
+		return true;
+	}
+
+	@Override
+	public int getComparatorInputOverride(World world, int x, int y, int z, int side) {
+		TileEntity tile = world.getTileEntity(x, y, z);
+		if(tile == null || !(tile instanceof TileTapeDrive)) {
+			return 0;
+		}
+		return ((TileTapeDrive) tile).getEnumState() == TapeDriveState.State.PLAYING ? 15 : 0;
 	}
 }
