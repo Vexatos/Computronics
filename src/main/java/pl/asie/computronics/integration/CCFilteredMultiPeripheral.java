21a6f3c6501d7ec86651399917d38582f7b2bcf6
Vexatos <Stuarzt@gmx.de>
2015-04-21 15:34:01 +0200

    Added Filtered Multiperipheral. Might be useful some day.

---
+++ src/main/java/pl/asie/computronics/integration/CCFilteredMultiPeripheral.java
@@ -0,0 +1,61 @@
+package pl.asie.computronics.integration;
+
+import dan200.computercraft.api.lua.ILuaContext;
+import dan200.computercraft.api.lua.LuaException;
+import dan200.computercraft.api.peripheral.IComputerAccess;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Vexatos
+ */
+public abstract class CCFilteredMultiPeripheral<T> extends CCMultiPeripheral<T> {
+	protected final List<String> methods;
+	protected List<String> availableMethods;
+
+	public CCFilteredMultiPeripheral() {
+		methods = Collections.emptyList();
+	}
+
+	public CCFilteredMultiPeripheral(T tile, String name, World world, int x, int y, int z) {
+		super(tile, name, world, x, y, z);
+		this.methods = getAllMethods();
+	}
+
+	protected abstract List<String> getAllMethods();
+
+	protected abstract boolean isMethodEnabled(String name);
+
+	protected boolean isMethodEnabled(int method) {
+		return isMethodEnabled(methods.get(method));
+	}
+
+	@Override
+	public String[] getMethodNames() {
+		availableMethods = new ArrayList<String>();
+		for(String method : methods) {
+			if(isMethodEnabled(method)) {
+				availableMethods.add(method);
+			}
+		}
+		return availableMethods.toArray(new String[availableMethods.size()]);
+	}
+
+	protected abstract Object[] call(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws LuaException, InterruptedException;
+
+	@Override
+	public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws LuaException, InterruptedException {
+		if(method < 0 || method >= availableMethods.size() || availableMethods.get(method) == null) {
+			throw new LuaException("not a valid function");
+		}
+
+		final int index = methods.indexOf(availableMethods.get(method));
+		if(index < 0 || index >= methods.size()) {
+			throw new LuaException("not a valid function");
+		}
+		return call(computer, context, index, arguments);
+	}
+}
