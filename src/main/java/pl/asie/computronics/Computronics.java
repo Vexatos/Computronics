eb18a57599f1db306c775ef006434bba7caf3a8c
Vexatos <Stuarzt@gmx.de>
2016-01-25 11:13:01 +0100

    Updated some mod dependencies.

--- src/main/java/pl/asie/computronics/Computronics.java
+++ src/main/java/pl/asie/computronics/Computronics.java
@@ -75,7 +75,7 @@ import java.util.concurrent.Executors;
 
 @Mod(modid = Mods.Computronics, name = Mods.Computronics_NAME, version = "@VERSION@",
 	dependencies = "required-after:asielib;required-after:Forge@[11.15.0.1718,);"
-		+ "after:ComputerCraft;after:OpenComputers@[1.5.18,);after:tis3d@[0.8.0.57,);"
+		+ "after:ComputerCraft;after:OpenComputers@[1.5.21,);after:tis3d@[0.8.3.12,);"
 		+ "before:OpenPeripheralCore@[1.1,);before:OpenPeripheralApi@[3.2,);"
 		+ "after:MineFactoryReloaded;after:RedLogic@[59.1.9,);after:ProjRed|Core;"
 		+ "after:BuildCraft|Core@[7.0.6,);after:Railcraft@[9.8.0.0,);"
