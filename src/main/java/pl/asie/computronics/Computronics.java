4521b5b8e00d620c52062dc88813c9ba7c7a671e
Vexatos <Stuarzt@gmx.de>
2016-01-28 15:59:16 +0100

    Updated Forge to recommended build.

--- src/main/java/pl/asie/computronics/Computronics.java
+++ src/main/java/pl/asie/computronics/Computronics.java
@@ -74,7 +74,7 @@ import java.util.concurrent.Executors;
 //import pl.asie.computronics.integration.railcraft.IntegrationRailcraft;
 
 @Mod(modid = Mods.Computronics, name = Mods.Computronics_NAME, version = "@VERSION@",
-	dependencies = "required-after:asielib;required-after:Forge@[11.15.0.1718,);"
+	dependencies = "required-after:asielib;required-after:Forge@[11.15.1.1722,);"
 		+ "after:ComputerCraft@[1.78,);after:OpenComputers@[1.5.21,);after:tis3d@[0.8.3.12,);"
 		+ "before:OpenPeripheralCore@[1.1,);before:OpenPeripheralApi@[3.2,);"
 		+ "after:MineFactoryReloaded;after:RedLogic@[59.1.9,);after:ProjRed|Core;"
