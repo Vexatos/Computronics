067c5e9a546314225b555a2144d0786597359c23
Vexatos <Stuarzt@gmx.de>
2016-05-09 18:50:19 +0200

    Fixes to decolorizer recipe.

---
+++ src/main/java/pl/asie/lib/util/FluidUtils.java
@@ -0,0 +1,27 @@
+package pl.asie.lib.util;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.IFluidContainerItem;
+
+/**
+ * @author Vexatos
+ */
+public class FluidUtils {
+
+	@SuppressWarnings("deprecation")
+	public static boolean containsFluid(ItemStack stack, Fluid fluid) {
+		Item item = stack.getItem();
+		FluidStack fstack = null;
+		if(item instanceof IFluidContainerItem) {
+			fstack = ((IFluidContainerItem) item).getFluid(stack);
+		}
+		if(fstack == null) {
+			fstack = FluidContainerRegistry.getFluidForFilledItem(stack);
+		}
+		return fstack != null && fstack.getFluid() == fluid;
+	}
+}
