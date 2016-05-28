0953d91b5e9f39553c0f43e3eacd57dd004ed253
Vexatos <Stuarzt@gmx.de>
2016-05-28 23:38:50 +0200

    Re-added EnderIO wrench support.

---
+++ src/main/java/pl/asie/lib/integration/tool/enderio/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author Vexatos
+ */
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package pl.asie.lib.integration.tool.enderio;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
