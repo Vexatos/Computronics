c30a940ab2504c7940a9e82444c9afcc63622ca0
Vexatos <Stuarzt@gmx.de>
2017-05-17 15:40:32 +0200

    Switched to properly registering sound events.

--- src/main/java/pl/asie/computronics/CommonProxy.java
+++ src/main/java/pl/asie/computronics/CommonProxy.java
@@ -10,6 +10,7 @@ import pl.asie.computronics.api.audio.AudioPacketRegistry;
 import pl.asie.computronics.audio.SoundCardPacket;
 import pl.asie.computronics.item.entity.EntityItemIndestructable;
 import pl.asie.computronics.reference.Mods;
+import pl.asie.computronics.reference.Sounds;
 import pl.asie.lib.network.Packet;
 
 import java.io.IOException;
@@ -44,7 +45,7 @@ public class CommonProxy {
 	}
 
 	public void init() {
-		//NO-OP
+		Sounds.registerSounds();
 	}
 
 	public void goBoom(Packet p) throws IOException {
