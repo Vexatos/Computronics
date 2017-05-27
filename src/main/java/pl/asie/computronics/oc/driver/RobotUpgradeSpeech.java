23c23405923614f9145dd806da0cebcf38c88859
Vexatos <Stuarzt@gmx.de>
2017-05-27 13:57:18 +0200

    Fixed Speech Box not properly stopping talking on chunk unload.

--- src/main/java/pl/asie/computronics/oc/driver/RobotUpgradeSpeech.java
+++ src/main/java/pl/asie/computronics/oc/driver/RobotUpgradeSpeech.java
@@ -121,6 +121,7 @@ public class RobotUpgradeSpeech extends ManagedEnvironmentWithComponentConnector
 		super.onDisconnect(node);
 		if(node == this.node()) {
 			isValid = false;
+			stopTalking();
 		}
 	}
 
